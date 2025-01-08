5e9beb22a4cee858d3c5752120a0ae5dd7055969
valennsalerno <salerno.valentin.99@gmail.com>
2025-01-08 12:52:17 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import * as middlewares from './middlewares';
+import api from './api';
+import jwt from 'jsonwebtoken';
+
+
+require('dotenv').config();
+
+const app = express();
+
+const secret = 'RhdGEiOnsiaWQ';
+const jwtTTL = Math.floor(Date.now() / 1000) + 60 * 60;
+
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Sirius People!  Register  Login</h1>');
+});
+
+app.post('/register', (req, res) => {
+  //To Do: sabe in DB
+  const userInDB = { id: 1, name: 'valen', admin: false };
+
+  res.json({ 
+    user: 'valen',
+    token: jwt.sign({ exp: jwtTTL, data: { id: userInDB.id } }, secret),
+  });
+});
+
+//app.post('/login', (req, res) => {});
+
+app.post('/upload', middlewares.auth, (req, res) => {
+  res.json({ message: 'File uploaded' });
+});
+
+app.get('/stats', middlewares.auth, (req, res) => {
+  res.json([
+    {
+      id: 1,
+      user: 'Valen',
+      files: 5,
+    },
+  ]);
+}); //show all the stats
+
+
+app.use('/api/v1', api);
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+
+export default app;
---
+++ src/middlewares.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from 'express';
+import ErrorResponse from './interfaces/ErrorResponse';
+import jwt from 'jsonwebtoken';
+
+const secret = 'RhdGEiOnsiaWQ';
+
+export function notFound(req: Request, res: Response, next: NextFunction) {
+  res.status(404);
+  const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+  next(error);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function errorHandler(err: Error, req: Request, res: Response<ErrorResponse>, next: NextFunction) {
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function auth(req: Request, res: Response, next: NextFunction) { 
+  if (!req.headers.authorization) {
+    return res.status(403).json({ error: 'Token required!' });
+  }
+
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, secret);
+    // remember the user validation if user exists in DB
+    next(decoded); // fix this
+  } catch (error) {
+    return res.status(403).json({ error: 'Invalid token!' });
+  }
+}
+
+//create auth middleware
+//use it in stats and upload
+//create BD and connect it -> mongo?
+//give a role for each user (add if its admin manually)
+//intentar hacer un login

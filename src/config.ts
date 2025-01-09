b409dc525aac8562fa69b8ccc3a98aaf09881dbb
valennsalerno <salerno.valentin.99@gmail.com>
2025-01-09 13:58:29 -0300

    Add some env

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
 export const {
   PORT = 3000,
 } = process.env;
-
-//desestructurarlo y parsearlo aca al ttl
--- src/routes/NoAuth.ts
+++ src/routes/NoAuth.ts
@@ -1,17 +1,31 @@
 import jwt from 'jsonwebtoken';
 import express from 'express';
+//import bcrypt from 'bcrypt';
 
 const router = express.Router();
-const secret = 'RhdGEiOnsiaWQ';
-const jwtTTL = Math.floor(Date.now() / 1000) + 60 * 60; //pasarlo al .env
-
+const secret = process.env.JWT_SECRET || 'defaultSecret';
+const jwtTTL = parseInt(process.env.JWT_TTL || '3600', 10);
 
 router.post('/login', (req, res) => {
   res.send('Login');
-});
+  //Validate input
+  /*
+  if (!username || !password ) {
+    return res.status(400).json({ message: 'Missing required fields' });
+  }
+  //Hash password
+  const hashedPassword : string = bcrypt.hash(password, salt:10);
 
+  //isPasswordCorrect
+
+  */
+});
+  
 router.post('/register', (req, res) => {
-  //To Do: save in DB
+  //To Do: Unique username
+  //Validate input
+  //Hash password
+  //Save in DB
   const userInDB = { id: 1, name: 'valen', admin: false };
 
   res.json({ 
